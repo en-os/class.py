a92ce79aa6da27c99e48cf2802947f07b51149a1
code-medicina <enosdreng@gmail.com>
2025-09-25 13:47:06 +0300

    Update class animal.py

--- class animal.py
+++ class animal.py
@@ -1,1 +1,25 @@
 
+class Animal:
+    def move(self):
+        return "Animal moves"
+
+class Bird(Animal):
+    def move(self):
+        return "Flying high!"
+
+class Fish(Animal):
+    def move(self):
+        return "Swimming deep!"
+
+class Dog(Animal):
+    def move(self):
+        return "Running fast!"
+
+class Snake(Animal):
+    def move(self):
+        return " Slithering silently!"
+
+# Test
+animals = [Bird(), Fish(), Dog(), Snake()]
+for animal in animals:
+    print(animal.move())
